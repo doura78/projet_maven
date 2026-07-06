37a17f6ff355a2db1af0390f23d6dd4cefca36bb
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-07-06 14:46:10 +0200

    projet_Maven_tp3_ex2

--- src/main/java/fr/diginamic/logging/App.java
+++ src/main/java/fr/diginamic/logging/App.java
@@ -4,9 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        LOG.info("Bonjour depuis SLF4J !");
+        LOGGER.info("Bonjour depuis SLF4J !");
     }
 }
